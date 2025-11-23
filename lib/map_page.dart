f154c6f4596a0680d619a3792670f8323fcf764e
Luis Fontan <luis.fontan1@upr.edu>
2025-11-23 14:59:28 -0400

    Implemented search bar widget with no function

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -136,6 +136,39 @@ class _MapPageState extends State<MapPage> {
           userAgentPackageName: 'com.example.oasis_frontend',
         ),
         CurrentLocationLayer(),
+        Padding(
+          padding: const EdgeInsets.only(top: 40),
+          child: SearchAnchor(
+            builder: (BuildContext context, SearchController controller) {
+              return SearchBar(
+                controller: controller,
+                padding: const WidgetStatePropertyAll<EdgeInsets>(
+                  EdgeInsets.symmetric(horizontal: 16.0),
+                ),
+                onTap: () {
+                  controller.openView();
+                },
+                onChanged: (_) {
+                  controller.openView();
+                },
+                leading: const Icon(Icons.search),
+              );
+            },
+            suggestionsBuilder: (BuildContext context, SearchController controller) {
+              return List<ListTile>.generate(5, (int index) {
+                final String item = 'item $index';
+                return ListTile(
+                  title: Text(item),
+                  onTap: () {
+                    setState(() {
+                      controller.closeView(item);
+                    });
+                  },
+                );
+              });
+            }, 
+          ),
+        ),
         Positioned(
           left: 16,
           bottom: 16,
