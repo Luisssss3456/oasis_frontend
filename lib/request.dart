1d5bd8b75382141d6527170027de960269684d1d
Luis Fontan <luis.fontan1@upr.edu>
2025-11-30 20:48:05 -0400

    Added  fetch path request file

---
+++ lib/request.dart
@@ -0,0 +1,43 @@
+// References:
+// https://stackoverflow.com/questions/58377795/how-to-pass-headers-in-the-http-post-request-in-flutter
+// https://stackoverflow.com/questions/50244416/how-to-pass-basic-auth-credentials-in-api-call-for-a-flutter-mobile-application
+// https://docs.flutter.dev/cookbook/networking/send-data
+
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<void> fetchPath() async {
+    final url  = Uri.parse("http://127.0.0.1:8000/routing/");
+    //final response = await http.get(url);
+
+    String username = 'your-user';
+    String password = 'your-password';
+    String basicAuth = 
+    'Basic ' + base64.encode(utf8.encode('$username:$password'));
+
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': "application/json",
+        'Authorization': basicAuth
+      },
+      body: jsonEncode({
+        "origin": {
+          "lat": 18.404520310646078,
+          "lng": -66.05455910070414
+          },
+        "destination": {
+          "lat": 18.39823227684525,
+          "lng": -66.04749513648974
+        }})
+      );
+
+
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      return responseData;
+    } else {
+      print("Error: ${response.statusCode}");
+    }
+}
